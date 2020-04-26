66855f4235265745e78058bc58449dc511da5526
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-04-26 01:24:54 +0100

    Used async/await

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
 const summaryEndpoint = "https://api.covid19api.com/summary";
 
+const global = document.querySelector(".summary-global");
+const newConfirmed = global.querySelector(".global__box--newconfirmed");
+const newDeaths = global.querySelector(".global__box--newdeaths");
+const newRecovered = global.querySelector(".global__box--newrecovered");
+const totalConfirmed = global.querySelector(".global__box--totalconfirmed");
+const totalDeaths = global.querySelector(".global__box--totaldeaths");
+const totalRecovered = global.querySelector(".lobal__box--totalrecovered");
 let summary = {};
 
-function fetchApi(apiLink) {
-  fetch(apiLink, {
-    method: "GET",
-  })
-    .then((blob) => blob.json())
-    .then((data) => {
-      summary = data;
-    })
-    .catch((err) => {
-      console.log(err);
-    });
+// function fetchApi(apiLink) {
+//   fetch(apiLink, {
+//     method: "GET",
+//   })
+//     .then((blob) => blob.json())
+//     .then((data) => {
+//       summary = data;
+//
+//       newConfirmed.textContent = summary.Global.NewConfirmed;
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
+// fetchApi(summaryEndpoint);
+
+// Used async/await
+async function fetchApi(apiLink) {
+  let response = await fetch(apiLink);
+  let data = await response.json();
+  return data;
 }
 
-fetchApi(summaryEndpoint);
+const promise = fetchApi(summaryEndpoint).then((data) => {
+  summary = data;
+  newConfirmed.textContent = summary.Global.NewConfirmed;
+});

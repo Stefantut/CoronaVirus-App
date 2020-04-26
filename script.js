18beebb6303a5183c38b58eea604a0593f56f9a1
Stefan Tutuianu <dragos_tutuianu@yahoo.com>
2020-04-26 01:43:47 +0100

    Displayed values

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 const summaryEndpoint = "https://api.covid19api.com/summary";
 
 const global = document.querySelector(".summary-global");
+console.log(global);
 const newConfirmed = global.querySelector(".global__box--newconfirmed");
 const newDeaths = global.querySelector(".global__box--newdeaths");
 const newRecovered = global.querySelector(".global__box--newrecovered");
 const totalConfirmed = global.querySelector(".global__box--totalconfirmed");
 const totalDeaths = global.querySelector(".global__box--totaldeaths");
-const totalRecovered = global.querySelector(".lobal__box--totalrecovered");
-let summary = {};
+const totalRecovered = global.querySelector(".global__box--totalrecovered");
+// let summary = {};
 
+// works with fetch api as well
 // function fetchApi(apiLink) {
 //   fetch(apiLink, {
 //     method: "GET",
@@ -16,7 +18,6 @@ let summary = {};
 //     .then((blob) => blob.json())
 //     .then((data) => {
 //       summary = data;
-//
 //       newConfirmed.textContent = summary.Global.NewConfirmed;
 //     })
 //     .catch((err) => {
@@ -32,7 +33,23 @@ async function fetchApi(apiLink) {
   return data;
 }
 
-const promise = fetchApi(summaryEndpoint).then((data) => {
-  summary = data;
-  newConfirmed.textContent = summary.Global.NewConfirmed;
-});
+// Updates all values
+function updateValues() {
+  fetchApi(summaryEndpoint).then((data) => {
+    let globalSummary = data.Global;
+    newConfirmed.textContent = globalSummary.NewConfirmed;
+    newDeaths.textContent = globalSummary.NewDeaths;
+    newRecovered.textContent = globalSummary.NewRecovered;
+    totalConfirmed.textContent = globalSummary.TotalConfirmed;
+    totalDeaths.textContent = globalSummary.TotalDeaths;
+    totalRecovered.textContent = globalSummary.TotalRecovered;
+
+    // console.log(Object.values(summary.Global));
+  });
+}
+
+// calls the function on page load
+updateValues();
+
+// calls the function every 5 minutes
+const updateApi = window.setInterval(updateValues, 50000);
